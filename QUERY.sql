62f6ee8de13e4d8568b296e635e1b5aa28a1200a
Saif Smran <23303079@iubat.edu>
2026-06-14 16:38:25 +0600

    Query 2 Done

--- QUERY.sql
+++ QUERY.sql
@@ -6,3 +6,13 @@ from
 where
     tournament_category = 'Champions League'
     and match_status = 'Available';
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+select
+    *
+from
+    users
+where
+    full_name ilike 'Tanvir%'
+    or full_name like '%Haque%';
+
