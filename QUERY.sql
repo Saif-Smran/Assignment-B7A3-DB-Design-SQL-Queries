0f07e167c05ff2af981e95ec444e0ac1461e6f88
Saif Smran <23303079@iubat.edu>
2026-06-14 17:05:30 +0600

    Query 6 Done

--- QUERY.sql
+++ QUERY.sql
@@ -46,3 +46,18 @@ SELECT
 FROM
     bookings
     RIGHT JOIN users u ON bookings.user_id = u.user_id;
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM
+    Bookings
+WHERE
+    total_cost > (
+        SELECT
+            AVG(total_cost)
+        FROM
+            Bookings
+    );
